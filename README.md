c77ea47e08d355ad70b925245823fa6956935a28
Theo <theo@iconara.net>
2010-11-05 22:40:00 +0100

    Added support for subscription

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ SNS
 Example:
 
     sns = Sns.new(aws_access_key, aws_secret_key)
-    sns.topic('arn:aws:sns:us-east-1:123456789012:test')
-    sns.publish('Hello', 'World')
+    topic = sns.topic('arn:aws:sns:us-east-1:123456789012:test')
+    topic.subscribe('email', 'someone@example.com')
+    topic.publish('Hello', 'World')
--- lib/sns/topic.rb
+++ lib/sns/topic.rb
@@ -8,7 +8,17 @@ class Sns
     end
     
     def publish(subject, message)
-      Request.new(@credentials).send('TopicArn' => @arn, 'Action' => 'Publish', 'Subject' => subject, 'Message' => message)
+      send_request('Action' => 'Publish', 'Subject' => subject, 'Message' => message)
+    end
+    
+    def subscribe(protocol, endpoint)
+      send_request('Action' => 'Subscribe', 'Protocol' => protocol, 'Endpoint' => endpoint)
+    end
+    
+  private
+  
+    def send_request(parameters)
+      Request.new(@credentials).send(parameters.merge('TopicArn' => @arn))
     end
   end
 end
